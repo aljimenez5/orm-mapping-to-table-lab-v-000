a8706cece1a3c44dcad0f7721e1d8cab35c80136
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-13 21:18:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   
   def save
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
-    self.id = 
+    self.id = DB[:conn].execute
   end
   
 end
